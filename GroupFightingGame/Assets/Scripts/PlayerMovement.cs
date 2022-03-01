b9a6290ff2de12f966889f13db19916db6937484
Cannencc <cannencc@gmail.com>
2022-02-28 20:12:01 -0500

    Sprite/Animation Testing
    Implementation of Movement and Animation:
    - colliders for platform and player sprite
    - movement in all planes
    - animations for running and jumping

---
+++ GroupFightingGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D body;
+    [SerializeField] private float speedH;
+    [SerializeField] private float speedJ;
+    private Animator anim;
+    private bool grounded;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        body = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speedH, body.velocity.y);
+        
+        
+        if(Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            Jump();
+        }
+
+        anim.SetBool("run", horizontalInput != 0);
+        anim.SetBool("jump", verticalInput != 0);
+        anim.SetBool("fall", body.velocity.y < 0);
+        anim.SetBool("grounded", grounded);
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speedJ);
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
+    }
+
+}
