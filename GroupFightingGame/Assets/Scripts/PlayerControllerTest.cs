b1894e8968bd88eae1eb83c65271156eb5d4f1d6
Cannencc <cannencc@gmail.com>
2022-04-11 21:32:56 -0400

    Character Movement
    - player has ability to jump and move using the WAD keys

--- GroupFightingGame/Assets/Scripts/PlayerControllerTest.cs
+++ GroupFightingGame/Assets/Scripts/PlayerControllerTest.cs
@@ -4,38 +4,42 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerControllerTest : MonoBehaviour
 {
-    private Rigidbody2D body;
+    public Rigidbody2D body;
     [SerializeField] private float speedH;
     [SerializeField] private float speedJ;
     private Animator anim;
     private bool grounded;
-    private Vector2 movementInput = Vector2.zero;
-    private void Awake()
-    {
-        //anim = GetComponent<Animator>();
-        body = GetComponent<Rigidbody2D>();
-    }
+    private float movementInput;
+
     public void OnMove(InputAction.CallbackContext context)
     {
-
-        movementInput = context.ReadValue<Vector2>();
-        /*anim.SetBool("run", horizontalInput != 0);
-        anim.SetBool("fall", body.velocity.y < 0);
-        anim.SetBool("grounded", grounded);*/
+        if(grounded)
+            movementInput = context.ReadValue<Vector2>().x;
     }
 
     public void OnJump(InputAction.CallbackContext context)
     {
         /*jumped = context.action.triggered;
         jumped = context.ReadValue<bool>();*/
-        body.velocity = new Vector2(body.velocity.x, speedJ);
+        Debug.Log("Grounded Status: " + grounded);
+        if(grounded)
+        {
+            body.velocity = new Vector2(body.velocity.x, speedJ);
+            grounded = false;
+        }
+
         //anim.SetTrigger("jump");
-        grounded = false;
+        //grounded = false;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Ground")
             grounded = true;
+    }
+    void Update()
+    {
+        Debug.Log("Grounded Status: " + grounded);
+        body.velocity = new Vector2(speedH * movementInput, body.velocity.y);
     }
 }
