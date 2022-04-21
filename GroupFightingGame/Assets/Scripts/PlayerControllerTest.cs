bceffcdb8ade6b46a494ffa8735fa93c1a500ba3
Cannencc <cannencc@gmail.com>
2022-04-20 20:42:47 -0400

    Updated Movement Speed
    - separated -x and +x movement into separate serialized variables

--- GroupFightingGame/Assets/Scripts/PlayerControllerTest.cs
+++ GroupFightingGame/Assets/Scripts/PlayerControllerTest.cs
@@ -6,8 +6,9 @@ public class PlayerControllerTest : MonoBehaviour
 {
     public Animator playerAnim;
     public Rigidbody2D body;
-    [SerializeField] private float speedH;
-    [SerializeField] private float speedJ;
+    [SerializeField] private float speedTowards;
+    [SerializeField] private float speedAway;
+    [SerializeField] private float speedJump;
     private Animator anim;
     private bool grounded, stop = false;
     private float movementInput;
@@ -33,7 +34,7 @@ public class PlayerControllerTest : MonoBehaviour
         Debug.Log("Grounded Status: " + grounded);
         if(grounded)
         {
-            body.velocity = new Vector2(body.velocity.x, speedJ);
+            body.velocity = new Vector2(body.velocity.x, speedJump);
             grounded = false;
         }
 
@@ -69,7 +70,11 @@ public class PlayerControllerTest : MonoBehaviour
     void Update()
     {
         //Debug.Log("Grounded Status: " + grounded);
-        body.velocity = new Vector2(speedH * movementInput, body.velocity.y);
+        if(movementInput < 0)
+            body.velocity = new Vector2(speedAway * movementInput, body.velocity.y);
+        else
+            body.velocity = new Vector2(speedTowards * movementInput, body.velocity.y);
+
         playerAnim.SetFloat("walkDirection", body.velocity.x);
     }
 }
