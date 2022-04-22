77b73882915d1a1aaebde620a5cf38776cd6e8d1
SarahJP5 <71723643+SarahJP5@users.noreply.github.com>
2022-04-21 20:53:15 -0400

    Archer character
    -idle animation
    -archer added to character select

--- GroupFightingGame/Assets/CharacterManager2.cs
+++ GroupFightingGame/Assets/CharacterManager2.cs
@@ -9,10 +9,23 @@ public class CharacterManager2 : MonoBehaviour
     public int selectedCharacter = 0;
 
     public GameObject[] skins;
-    public int selectedSkin = 1;
+    public int selectedSkin = 0;
+
+    public void Start()
+    {
+        skins[0].SetActive(false); 
+        skins[1].SetActive(false); 
+        characters[1].SetActive(false);
+    }
 
     public void Next()
     {
+        if(selectedSkin == 1)
+        {
+            selectedSkin = 0; 
+            skins[selectedCharacter].SetActive(false);
+        }
+       
         characters[selectedCharacter].SetActive(false);
         selectedCharacter = (selectedCharacter + 1) % characters.Length;
         characters[selectedCharacter].SetActive(true); 
@@ -20,6 +33,13 @@ public class CharacterManager2 : MonoBehaviour
 
     public void Previous()
     {
+
+        if(selectedSkin == 1)
+        {
+            selectedSkin = 0; 
+            skins[selectedCharacter].SetActive(false);
+        }
+
         characters[selectedCharacter].SetActive(false);
         selectedCharacter--;
         if(selectedCharacter < 0)
