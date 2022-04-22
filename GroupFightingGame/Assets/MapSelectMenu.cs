71bd5e926054dd18030984212244decfe89dc456
stevenhorn17 <stevenhorn02@gmail.com>
2022-04-22 14:51:50 -0400

    Sound Design
    Added Sound and Music to levels and menus. Buttons have clicks and music should start and restart at the right times. Navigating menus should also not cause audio issues.

--- GroupFightingGame/Assets/MapSelectMenu.cs
+++ GroupFightingGame/Assets/MapSelectMenu.cs
@@ -7,16 +7,23 @@ public class MapSelectMenu : MonoBehaviour
 {
     public void Map1 ()
     {
+        AudioManager.audioManagerInst.GetComponent<AudioSource>().Pause();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        battleMusic.instance.GetComponent<AudioSource>().Play();
     }
 
     public void Map2()
     {
+        AudioManager.audioManagerInst.GetComponent<AudioSource>().Pause();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        battleMusic.instance.GetComponent<AudioSource>().Play();
+
     }
 
     public void Map3()
     {
+        AudioManager.audioManagerInst.GetComponent<AudioSource>().Pause();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+        battleMusic.instance.GetComponent<AudioSource>().Play();
     }
 }
